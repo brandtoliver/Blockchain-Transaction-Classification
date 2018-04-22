b89562dce455d5c3469b86299bbf8ec26fb8da09
mar10nJohns1 <martin@johnsenprivat.dk>
2018-04-22 09:51:05 +0200

    Updated model

--- Models/Mixture_models.py
+++ Models/Mixture_models.py
@@ -12,16 +12,14 @@ import tensorflow as tf
 import os
 import pandas as pd
 
+from edward.models import Categorical
+
 from edward.models import (
     Categorical, Dirichlet, Empirical, InverseGamma,
     MultivariateNormalDiag, Normal, ParamMixture, RandomVariable, Bernoulli)
 from tensorflow.contrib.distributions import Distribution
 
 plt.style.use('ggplot')
-
-
-""" Notes til data: ikke standardiseret, hvilket skal huskes før det giver mening
-"""
 
 """
 Notes til Bernouli: logits: An N-D `Tensor` representing the log-odds of a `1` event. Each
@@ -32,68 +30,32 @@ Notes til Bernouli: logits: An N-D `Tensor` representing the log-odds of a `1` e
 
 """
 
-"""
-#Building dataset:
-def build_toy_dataset(N):
-  pi = np.array([0.4, 0.6])
-  mus = [[1, 1], [-1, -1]]
-  stds = [[0.1, 0.1], [0.1, 0.1]]
-  x = np.zeros((N, 2), dtype=np.float32)                                #N*2 matrix all zeros
-  for n in range(N):
-    k = np.argmax((np.random.multinomial1, pi))                         #np.random.multinomial, gives a vector, of which of the two (0.4 , 0.6) that was chosen in 1 draw: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.random.multinomial.html
-                                                                        #np.argmax gives the index of the biggest number from the output array. In this case, it will
-                                                                        #always be 1, because it only makes 1 draw, and therefore none will be chosen 2 or more.
+#Reading subset:
+currDir = os.getcwd()                                                   #Defining the current directory
+fileSep = os.sep                                                        #Defining filesep
+file_path = os.path.join (currDir + fileSep + "Data" + fileSep)         #Defining the folder where the files are located:
 
-    x[n, :] = np.random.multivariate_normal(mus[k], np.diag(stds[k]))   #Making row n into a random draw from the multivariate normal distribution.
-                                                                        #The multivariate normal distribution is a generalisation of the 1-Dimensional normal distribution.
-                                                                        #The row is therefore a draw of a two points, taken from the random multivariate ND.
-                                                                        #np.random.multivariate_normal is specified by its mean and covariance matrix (mus and stds): https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.random.multivariate_normal.html
-                                                                        #Second argument makes a diagonal matrix from stds[1]
+sub= os.path.join(file_path, 'user_rec.csv')                            #Path to subset
+subset= pd.read_csv(sub, sep=",", header=0)                             #Reading subset
+x_trainn=subset.values                                                   #Redefine x_train
 
-  return x                                                              #x is now a N*2 matrix, composed of random drawn datapoints from the multivariate distribution
+#Standardizing:
+from sklearn import preprocessing
+X= preprocessing.scale(x_trainn)
 
-
-N = 500  # number of data points                                        #Setting parameters - N is defined from the number of rows
-K = 2  # number of components                                           #Setting parameters - number of clusters
-D = 2  # dimensionality of data                                         #Setting parameters - dimension of data
-ed.set_seed(42)
-
-x_train = build_toy_dataset(N)                                          #Creating toy-data of type: numpy.ndarray
-
-#Plotting x_train data. Remember x_train holds two positional datapoints, which is why x_train[:,0] and x_train[:,1] gets plotted.
-plt.scatter(x_train[:, 0], x_train[:, 1])
-plt.axis([-3, 3, -3, 3])
-plt.title("Simulated dataset")
-plt.show()
-"""
-"""
-The plot shows exactly the intention of line 29. Creating seemingly random draws from a multivariate normal distribution with a mean randomly chosen
-from mus through pi, but with a bias to mus[1] because this one hold the largest probability c.f. pi. 
-"""
-
-
-#Defining the current directory
-currDir = os.getcwd()
-#Defining filesep
-fileSep = os.sep
-#Defining the folder where the files are located:
-file_path = os.path.join (currDir + fileSep + "Documents" + fileSep + "blockchain_new_2013" + fileSep)        #Defining the folder where the files are located:
-
-sub= os.path.join(file_path, 'user_rec.csv')                     #Path to subset
-subset= pd.read_csv(sub, sep=",")                             #Reading subset
-x_train=subset.values                                                   #Redefine x_train
 #x_train= x_train[:,:13]
-x_train= x_train[4000:5000,]
-
+x_train= X[4900:5000,]
+x_test= X[4800:4900,]
+N_testt= len(x_test)
 
 N = len(x_train)  # number of data points                               #Setting parameters - N is defined from the number of rows
-K = 5  # number of components                                           #Setting parameters - number of clusters
+K = 10  # number of components                                           #Setting parameters - number of clusters
 D = x_train.shape[1]  # dimensionality of data                           #Setting parameters - dimension of data
 ed.set_seed(42)
 
 
 
-""" PRIORS """
+
 
 #Model:
 pi = Dirichlet(tf.ones(K))                                              #Defining prior
@@ -104,12 +66,12 @@ x = ParamMixture(pi, {'loc': mu, 'scale_diag': tf.sqrt(sigmasq)},       #Given a
                  sample_shape=N)
 z = x.cat                                                               #z is now defined as the component prescribed to the observed variable x.
 
-""" PRIORS """
+
 
 
 #Inference:                                                             #a conclusion reached on the basis of evidence and reasoning
-T = 10000                                                                #number of MCMC samples
-qpi = Empirical(                                                        #Emperical is just a sample of a set, which is good enough representation of the whole set.   
+T = 500                                                                #number of MCMC samples
+qpi = Empirical(                                                        #Emperical is just a sample of a set, which is good enough representation of the whole set.
     tf.get_variable(                                                    #Gets an existing variable with these parameters or create a new one.
     "qpi/params", [T, K],                                               #Setting shape to be Number of MCMC samples times number of components
     initializer=tf.constant_initializer(1.0 / K)))                      #Initializer that generates tensors with constant values
@@ -159,23 +121,10 @@ sigmasq_sample = qsigmasq.sample(100)
 x_post = Normal(loc=tf.ones([N, 1, 1, 1]) * mu_sample,
                 scale=tf.ones([N, 1, 1, 1]) * tf.sqrt(sigmasq_sample))
 x_broadcasted = tf.tile(tf.reshape(x_train, [N, 1, 1, D]), [1, 100, K, 1])
-"""
-Originalt data:
-x_post
-Out[7]: <ed.RandomVariable 'Normal_1/' shape=(500, 100, 2, 2) dtype=float32>
-x_broadcasted
-Out[8]: <tf.Tensor 'Tile:0' shape=(500, 100, 2, 2) dtype=float32>
-
-
-Med subset:
-x_post
-Out[14]: <ed.RandomVariable 'Normal_1/' shape=(500, 100, 2, 15) dtype=float32>
-x_broadcasted
-Out[15]: <tf.Tensor 'Tile:0' shape=(500, 100, 2, 15) dtype=float64>
-"""
 
 #x_post = tf.cast(x_post, tf.float64)                                        #Laves om til float64
 x_broadcasted = tf.cast(x_broadcasted, tf.float32)                          #Laves om til float64
+
 
 # Sum over latent dimension, then average over posterior samples.
 # ``log_liks`` ends up with shape (N, K).
@@ -183,14 +132,62 @@ log_liks = x_post.log_prob(x_broadcasted)
 log_liks = tf.reduce_sum(log_liks, 3)                                       #Optimization step for sum
 log_liks = tf.reduce_mean(log_liks, 1)
 
+
+#1
+x_neg_log_prob = (-tf.reduce_sum(x_post.log_prob(x_broadcasted)) /
+                    tf.cast(tf.shape(x_broadcasted)[0], tf.float32))
+x_neg_log_prob.eval()
+
+#2
+a=tf.reduce_sum(log_liks)
+a.eval()
+
+"""
+K=10:
+-12720.996 (stand)
+
+K=50:
+-70788.31 (stand)
+
+K=500:
+-1.1097861e+26
+
+"""
+
+
 # Choose the cluster with the highest likelihood for each data point.
 clusters = tf.argmax(log_liks, 1).eval()
 
+
+
+#Criticism
+inference = ed.Gibbs({pi: qpi, mu: qmu, sigmasq: qsigmasq, z: qz},
+                     data={x: x_train})
+
+
+x_post = ed.copy(x, {pi: qpi, mu: qmu, sigmasq: qsigmasq, z: qz})
+
+x_test = tf.cast(x_test, tf.float32)
+ed.evaluate('log_likelihood', data={x_post: x_test})
+"""
+K=10:
+-148.55351
+
+K=50:
+-29.601395 (stand)
+
+-2.8144713e+20
+
+K=500:
+-5.6210855e+18
+
+"""
+
 #Plot the clusters:
-plt.scatter(x_train[:, 5], x_train[:, 6], c=clusters, cmap=cm.bwr)
-plt.axis([-5, 65, -5, 45])
-plt.title("Predicted cluster assignments")
-plt.show()
+#plt.scatter(x_train[:, 5], x_train[:, 6], c=clusters, cmap=cm.bwr)
+#plt.axis([-5, 100, -5, 100])
+#plt.title("Predicted cluster assignments")
+#plt.show()
 
 
 #Next: Find ud af hvorfor når du plotter andre variable end x_train[:,0] og x_train[:,1] så kommer der ikke farver på plottet
@@ -201,3 +198,24 @@ Finde ud af hvordan vi finder det optimale antal clustre
 
 
 """
+
+
+###Model predictions:
+
+# create local posterior factors for test data, assuming test data
+# has N_test many data points
+qz_test = Categorical(logits=tf.Variable(tf.zeros([N_testt, K])))
+
+# run local inference conditional on global factors
+inference_test = ed.Inference({z: qz_test}, data={x: x_test, pi: qpi, mu: qmu, sigmasq: qsigmasq})
+inference_test.run()
+
+# build posterior predictive on test data
+x_post = ed.copy(x, {z: qz_test, beta: qbeta}})
+ed.evaluate('log_likelihood', data={x_post: x_test})
+
+
+
+{pi: qpi, mu: qmu, sigmasq: qsigmasq, z: qz}
+
+x_post = ed.copy(x, {pi: qpi, mu: qmu, sigmasq: qsigmasq, z: qz})
